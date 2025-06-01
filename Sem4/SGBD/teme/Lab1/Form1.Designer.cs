48709c26538156eff8f9ece5de9f1d07edb8b34b
bxpxi <155725673+bxpxi@users.noreply.github.com>
2025-06-01 17:41:02 +0300

    Add files via upload

---
+++ Sem4/SGBD/teme/Lab1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace Lab1_sper_ultim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.lblNume = new System.Windows.Forms.Label();
+            this.lblPrenume = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblGrad = new System.Windows.Forms.Label();
+            this.txtNume = new System.Windows.Forms.TextBox();
+            this.txtPrenume = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtGrad = new System.Windows.Forms.TextBox();
+            this.btnAdaugaMedic = new System.Windows.Forms.Button();
+            this.btnActualizeazaMedic = new System.Windows.Forms.Button();
+            this.btnStergeMedic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(27, 73);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 62;
+            this.dataGridViewParent.RowTemplate.Height = 28;
+            this.dataGridViewParent.Size = new System.Drawing.Size(490, 423);
+            this.dataGridViewParent.TabIndex = 0;
+            this.dataGridViewParent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewParent_CellClick);
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(568, 71);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 62;
+            this.dataGridViewChild.RowTemplate.Height = 28;
+            this.dataGridViewChild.Size = new System.Drawing.Size(466, 425);
+            this.dataGridViewChild.TabIndex = 1;
+            this.dataGridViewChild.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewChild_CellClick);
+            // 
+            // lblNume
+            // 
+            this.lblNume.AutoSize = true;
+            this.lblNume.Location = new System.Drawing.Point(1076, 76);
+            this.lblNume.Name = "lblNume";
+            this.lblNume.Size = new System.Drawing.Size(55, 20);
+            this.lblNume.TabIndex = 2;
+            this.lblNume.Text = "Nume:";
+            // 
+            // lblPrenume
+            // 
+            this.lblPrenume.AutoSize = true;
+            this.lblPrenume.Location = new System.Drawing.Point(1076, 152);
+            this.lblPrenume.Name = "lblPrenume";
+            this.lblPrenume.Size = new System.Drawing.Size(77, 20);
+            this.lblPrenume.TabIndex = 3;
+            this.lblPrenume.Text = "Prenume:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(1079, 220);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(52, 20);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(1079, 280);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(66, 20);
+            this.lblTelefon.TabIndex = 5;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // lblGrad
+            // 
+            this.lblGrad.AutoSize = true;
+            this.lblGrad.Location = new System.Drawing.Point(1082, 340);
+            this.lblGrad.Name = "lblGrad";
+            this.lblGrad.Size = new System.Drawing.Size(49, 20);
+            this.lblGrad.TabIndex = 6;
+            this.lblGrad.Text = "Grad:";
+            // 
+            // txtNume
+            // 
+            this.txtNume.Location = new System.Drawing.Point(1165, 73);
+            this.txtNume.Name = "txtNume";
+            this.txtNume.Size = new System.Drawing.Size(168, 26);
+            this.txtNume.TabIndex = 7;
+            // 
+            // txtPrenume
+            // 
+            this.txtPrenume.Location = new System.Drawing.Point(1165, 146);
+            this.txtPrenume.Name = "txtPrenume";
+            this.txtPrenume.Size = new System.Drawing.Size(168, 26);
+            this.txtPrenume.TabIndex = 8;
+            this.txtPrenume.TextChanged += new System.EventHandler(this.txtPrenume_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(1165, 214);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(168, 26);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(1165, 274);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(168, 26);
+            this.txtTelefon.TabIndex = 10;
+            // 
+            // txtGrad
+            // 
+            this.txtGrad.Location = new System.Drawing.Point(1165, 337);
+            this.txtGrad.Name = "txtGrad";
+            this.txtGrad.Size = new System.Drawing.Size(168, 26);
+            this.txtGrad.TabIndex = 11;
+            // 
+            // btnAdaugaMedic
+            // 
+            this.btnAdaugaMedic.Location = new System.Drawing.Point(1190, 440);
+            this.btnAdaugaMedic.Name = "btnAdaugaMedic";
+            this.btnAdaugaMedic.Size = new System.Drawing.Size(94, 30);
+            this.btnAdaugaMedic.TabIndex = 12;
+            this.btnAdaugaMedic.Text = "ADD";
+            this.btnAdaugaMedic.UseVisualStyleBackColor = true;
+            this.btnAdaugaMedic.Click += new System.EventHandler(this.btnAdaugaMedic_Click);
+            // 
+            // btnActualizeazaMedic
+            // 
+            this.btnActualizeazaMedic.Location = new System.Drawing.Point(1190, 501);
+            this.btnActualizeazaMedic.Name = "btnActualizeazaMedic";
+            this.btnActualizeazaMedic.Size = new System.Drawing.Size(94, 29);
+            this.btnActualizeazaMedic.TabIndex = 13;
+            this.btnActualizeazaMedic.Text = "UPDATE";
+            this.btnActualizeazaMedic.UseVisualStyleBackColor = true;
+            this.btnActualizeazaMedic.Click += new System.EventHandler(this.btnActualizeazaMedic_Click);
+            // 
+            // btnStergeMedic
+            // 
+            this.btnStergeMedic.Location = new System.Drawing.Point(1190, 564);
+            this.btnStergeMedic.Name = "btnStergeMedic";
+            this.btnStergeMedic.Size = new System.Drawing.Size(94, 32);
+            this.btnStergeMedic.TabIndex = 14;
+            this.btnStergeMedic.Text = "DELETE";
+            this.btnStergeMedic.UseVisualStyleBackColor = true;
+            this.btnStergeMedic.Click += new System.EventHandler(this.btnStergeMedic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 662);
+            this.Controls.Add(this.btnStergeMedic);
+            this.Controls.Add(this.btnActualizeazaMedic);
+            this.Controls.Add(this.btnAdaugaMedic);
+            this.Controls.Add(this.txtGrad);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPrenume);
+            this.Controls.Add(this.txtNume);
+            this.Controls.Add(this.lblGrad);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPrenume);
+            this.Controls.Add(this.lblNume);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.Label lblNume;
+        private System.Windows.Forms.Label lblPrenume;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblGrad;
+        private System.Windows.Forms.TextBox txtNume;
+        private System.Windows.Forms.TextBox txtPrenume;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtGrad;
+        private System.Windows.Forms.Button btnAdaugaMedic;
+        private System.Windows.Forms.Button btnActualizeazaMedic;
+        private System.Windows.Forms.Button btnStergeMedic;
+    }
+}
+
---
+++ Sem4/SGBD/teme/Lab1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_sper_ultim
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=Patricia\SQLEXPRESS;Database=ClinicaMedicala;
+        Integrated Security=true;TrustServerCertificate=true;";
+        DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter = new SqlDataAdapter();
+        SqlDataAdapter childAdapter = new SqlDataAdapter();
+        BindingSource parentBS = new BindingSource();
+        BindingSource childBS = new BindingSource();
+
+        private int specializareSelectata = 0;
+        private int medicSelectat = 0;
+        private string numeMedic;
+        private string prenumeMedic;
+        private string emailMedic;
+        private string telefonMedic;
+        private string gradMedic;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void refreshDataGridView()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Specializari;",
+                        connection);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Medici;",
+                        connection);
+                    ds.Clear();
+                    parentAdapter.Fill(ds, "Specializari");
+                    childAdapter.Fill(ds, "Medici");
+                    parentBS.DataSource = ds.Tables["Specializari"];
+                    dataGridViewParent.DataSource = parentBS;
+                    dataGridViewChild.DataSource = childBS;
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewParent.Rows[e.RowIndex];
+                int.TryParse(row.Cells[0].Value.ToString(), out specializareSelectata);
+            }
+            txtNume.Text = "";
+            txtPrenume.Text = "";
+            txtEmail.Text = "";
+            txtTelefon.Text = "";
+            txtGrad.Text = "";
+        }
+
+        private void dataGridViewChild_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewChild.Rows[e.RowIndex];
+                int.TryParse(row.Cells[0].Value.ToString(), out medicSelectat);
+                numeMedic = row.Cells[1].Value.ToString();
+                prenumeMedic = row.Cells[2].Value.ToString();
+                emailMedic = row.Cells[5].Value.ToString();
+                telefonMedic = row.Cells[4].Value.ToString();
+                gradMedic = row.Cells[7].Value.ToString();
+            }
+            txtNume.Text = numeMedic;
+            txtPrenume.Text = prenumeMedic;
+            txtEmail.Text = emailMedic;
+            txtTelefon.Text = telefonMedic;
+            txtGrad.Text = gradMedic;
+        }
+
+        private void txtPrenume_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdaugaMedic_Click(object sender, EventArgs e)
+        {
+            if (specializareSelectata == 0)
+            {
+                MessageBox.Show("Alegeti o specializare pentru medic.");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand insertCommand = new SqlCommand("INSERT INTO Medici(Nume, Prenume, ID_specializare," +
+                                   "Email, Telefon, Data_angajarii, Grad_profesional) VALUES (@nume, @prenume, @id_specializare, @email, @telefon, @data, @grad);", connection);
+                        insertCommand.Parameters.AddWithValue("@nume", txtNume.Text);
+                        insertCommand.Parameters.AddWithValue("@prenume", txtPrenume.Text);
+                        insertCommand.Parameters.AddWithValue("@id_specializare", specializareSelectata);
+                        insertCommand.Parameters.AddWithValue("@email", txtEmail.Text);
+                        insertCommand.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+                        insertCommand.Parameters.AddWithValue("@data", DateTime.Now);
+                        insertCommand.Parameters.AddWithValue("@grad", txtGrad.Text);
+                        MessageBox.Show($"Valoarea selectată este: {specializareSelectata}");
+                        int inserRowCount = insertCommand.ExecuteNonQuery();
+                        MessageBox.Show("Medicul a fost adaugat cu succes!");
+                        txtNume.Text = "";
+                        txtPrenume.Text = "";
+                        txtEmail.Text = "";
+                        txtTelefon.Text = "";
+                        txtGrad.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnStergeMedic_Click(object sender, EventArgs e)
+        {
+            if (medicSelectat == 0)
+            {
+                MessageBox.Show("Alegeti un medic.");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand deleteCommand = new SqlCommand("DELETE FROM Medici WHERE ID_medic=@id;", connection);
+                        deleteCommand.Parameters.AddWithValue("@id", medicSelectat);
+                        int deleteRowCount = deleteCommand.ExecuteNonQuery();
+                        MessageBox.Show("Medicul a fost sters cu succes!");
+                        txtNume.Text = "";
+                        txtPrenume.Text = "";
+                        txtEmail.Text = "";
+                        txtTelefon.Text = "";
+                        txtGrad.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnActualizeazaMedic_Click(object sender, EventArgs e)
+        {
+            if (medicSelectat == 0)
+            {
+                MessageBox.Show("Alegeti un medic.");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand updateCommand = new SqlCommand("UPDATE Medici SET Nume=@nume, Prenume=@prenume, Email=@email, Telefon=@telefon, Grad_profesional=@grad WHERE ID_medic=@id;", connection);
+                        updateCommand.Parameters.AddWithValue("@nume", txtNume.Text);
+                        updateCommand.Parameters.AddWithValue("@prenume", txtPrenume.Text);
+                        updateCommand.Parameters.AddWithValue("@email", txtEmail.Text);
+                        updateCommand.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+                        updateCommand.Parameters.AddWithValue("@grad", txtGrad.Text);
+                        updateCommand.Parameters.AddWithValue("@id", medicSelectat);
+                        int updateRowCount = updateCommand.ExecuteNonQuery();
+                        MessageBox.Show("Medicul a fost actualizat cu succes!");
+                        txtNume.Text = "";
+                        txtPrenume.Text = "";
+                        txtEmail.Text = "";
+                        txtTelefon.Text = "";
+                        txtGrad.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Specializari;",
+                        connection);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Medici;",
+                        connection);
+                    parentAdapter.Fill(ds, "Specializari");
+                    childAdapter.Fill(ds, "Medici");
+                    parentBS.DataSource = ds.Tables["Specializari"];
+                    dataGridViewParent.DataSource = parentBS;
+                    DataColumn parentColumn = ds.Tables["Specializari"].Columns["ID_specializare"];
+                    DataColumn childColumn = ds.Tables["Medici"].Columns["ID_specializare"];
+                    DataRelation relation = new DataRelation("FK_Specializari_Medici",
+                        parentColumn, childColumn);
+                    ds.Relations.Add(relation);
+                    childBS.DataSource = parentBS;
+                    childBS.DataMember = "FK_Specializari_Medici";
+                    dataGridViewChild.DataSource = childBS;
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
